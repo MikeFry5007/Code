263ed4e791ceb42a5a7f8419b3109322a9f3ee95
MikeFry5007 <107890178+MikeFry5007@users.noreply.github.com>
2022-07-14 14:55:40 -0400

    Create Flow: action script.js

---
+++ Flow: action script.js
@@ -0,0 +1,9 @@
+(function execute(inputs, outputs) {
+  
+  
+if (inputs.value < 8){
+  outputs.labors_hours_results = true;
+} else {
+      outputs.labors_hours_results = false;
+}
+  })(inputs, outputs);
